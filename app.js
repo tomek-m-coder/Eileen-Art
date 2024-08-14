a473b506d125419098698b57a03d2b716c205aa9
tomek-m-coder <60541005+tomek-m-coder@users.noreply.github.com>
2024-08-14 14:39:51 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
-// Funkcja do zmiany tła
 function changeBackground(color) {
     switch (color) {
         case "1":
-            document.body.style.backgroundColor = "#f1f1f2";
+            document.body.style.backgroundColor = "#a2c2e6";
             break;
         case "2":
-            document.body.style.backgroundColor = "#45f684";
+            document.body.style.background = "linear-gradient(to bottom, #ffb347, #ffcc33)";
             break;
         case "3":
-            document.body.style.backgroundColor = "#faffd1";
+            document.body.style.background = "linear-gradient(to bottom, #a8e0ff, #b3f2ff)";
             break;
     }
 }
 
-// Funkcja do przełączania widoczności menu nawigacyjnego
 function toggleNav() {
     const sideNav = document.getElementById("sideNav");
     if (sideNav.style.width === "250px" || sideNav.style.width === "30%") {
